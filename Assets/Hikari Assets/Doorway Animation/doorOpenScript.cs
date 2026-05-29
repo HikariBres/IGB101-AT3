2195ab7861a8b72f7ec6c513223efa931b2a4359
Hikari Bresnan <hikaribres@gmail.com>
2026-05-29 15:34:09 +1000

    3:33 29/05 Hikari
    fixed door script

--- Assets/Hikari Assets/Doorway Animation/doorOpenScript.cs
+++ Assets/Hikari Assets/Doorway Animation/doorOpenScript.cs
@@ -5,19 +5,20 @@ public class doorOpenScript : MonoBehaviour
 {
     public doorTriggerScript doorScript;
     public Animation anim;
+    public bool doorOpened = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start () {
+    void Start() {
         doorScript = GameObject.FindGameObjectWithTag("doorTrigger").GetComponent<doorTriggerScript>();
         anim = gameObject.GetComponent<Animation>();
         }
-
-    void update (){
-        //if (doorScript.openDoorBool == false){
-            //anim.Play("doorOpen");
-        //}
-
+    
+    void Update(){
+        if(doorScript.openDoorBool == true){
+            if(doorOpened == false){
+                anim.Play("doorOpen");
+                doorOpened = true;
+            }
+        }
     }
 
-
-    
 }
